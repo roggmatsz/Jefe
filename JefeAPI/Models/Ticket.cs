e90541a890091f53ab22991b4cf67daa0b45d6ea
Rogger Matamoros <roggmatz@gmail.com>
2017-07-05 13:13:59 -0400

    Inital commit. Created Ticket POCO class.

---
+++ JefeAPI/Models/Ticket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JefeAPI.Models
+{
+    public class Ticket {
+        public int id;
+        public int GameID { get; set; }
+        public int Slot { get; set; }
+        public int Value { get; set; }
+    }
+}

93d565e53f4302b7d617f552077b30ac803f723b
Rogger Matamoros <roggmatz@gmail.com>
2017-07-05 14:07:44 -0400

    Enabled Context and InMemory provider in ConfigureServices()

--- JefeAPI/Startup.cs
+++ JefeAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace JefeAPI
 {
@@ -29,6 +30,7 @@ namespace JefeAPI
         {
             // Add framework services.
             services.AddMvc();
+            services.AddDbContext<Models.ApiContext>(opt => opt.UseInMemoryDatabase());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

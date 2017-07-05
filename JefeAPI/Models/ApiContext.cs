00f90673bc6ff0693e8e157c0e86b1f37768825a
Rogger Matamoros <roggmatz@gmail.com>
2017-07-05 14:03:28 -0400

    Initial commit. No modifications from tutorial apart from adaptation to problem context.

---
+++ JefeAPI/Models/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using JefeAPI.Models;
+
+namespace JefeAPI.Models
+{
+    public class ApiContext : DbContext {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) 
+        {}
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Day> Days { get; set; }
+        public DbSet<Tally> Tallies { get; set; }
+    }
+}

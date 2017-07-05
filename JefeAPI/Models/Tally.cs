31e72f12a4351622975025d8304bfe507cbb4a90
Rogger Matamoros <roggmatz@gmail.com>
2017-07-05 13:30:45 -0400

    Initial Commit, Tally POCO.

---
+++ JefeAPI/Models/Tally.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JefeAPI.Models
+{
+    public class Tally {
+        public int id;
+        public int DayID { get; set; }
+        public int TicketID { get; set; }
+        public int Start { get; set; }
+        public int End { get; set; }
+        public int Count { get; }
+        public int Sales { get; }
+    }
+}

f77bf25294fbaef47eefaa7d253244362ef15873
Rogger Matamoros <roggmatz@gmail.com>
2017-07-05 13:26:42 -0400

    Initial commit, Day POCO.

---
+++ JefeAPI/Models/Day.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JefeAPI.Models
+{
+    public class Day {
+        public int id;
+        public DateTime Date { get; set; }
+        //tallies array here
+        public int TicketsSold { get; set; }
+        public int Sales { get; set; }
+    }
+}

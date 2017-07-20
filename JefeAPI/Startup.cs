3a40ef61269471e9a7a365f0cb204bacdb5e216e
Rogger Matamoros <roggmatz@gmail.com>
2017-07-20 10:55:48 -0400

    Registered Swashbuckle generator as a service.

--- JefeAPI/Startup.cs
+++ JefeAPI/Startup.cs
@@ -31,6 +31,12 @@ namespace JefeAPI
             // Add framework services.
             services.AddMvc();
             services.AddDbContext<Models.ApiContext>(opt => opt.UseInMemoryDatabase());
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info {
+                    Title = "JefeAPI",
+                    Version = "v1"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

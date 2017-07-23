2d1d6ecf96888b1e2d5d896c6ce4525ee55676d4
Rogger Matamoros <roggmatz@gmail.com>
2017-07-23 09:38:52 -0400

    Removed id property and constructor to designate model for client/server comms.

--- JefeAPI/Models/Ticket.cs
+++ JefeAPI/Models/Ticket.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace JefeAPI.Models
 {
-    public class Ticket {
-        public int id { get; set; }
+    public class Ticket {       
         public int GameID { get; set; }
         public int Slot { get; set; }
         public int Value { get; set; }
-        
-        public Ticket() {}
     }
 }

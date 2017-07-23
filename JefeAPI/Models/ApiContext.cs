923e93553f5dbbf1e9da246aae94776a1599fe41
Rogger Matamoros <roggmatz@gmail.com>
2017-07-23 12:23:42 -0400

    Refactored to use TicketEntity instead of Ticket as the DB model. Temporarily commented out Days and Tallies.

--- JefeAPI/Models/ApiContext.cs
+++ JefeAPI/Models/ApiContext.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-using JefeAPI.Models;
+using JefeAPI.Entities;
 
 namespace JefeAPI.Models
 {
     public class ApiContext : DbContext {
         public ApiContext(DbContextOptions<ApiContext> options) : base(options) 
         {}
-        public DbSet<Ticket> Tickets { get; set; }
-        public DbSet<Day> Days { get; set; }
-        public DbSet<Tally> Tallies { get; set; }
+        public DbSet<TicketEntity> Tickets { get; set; }
+        //public DbSet<Day> Days { get; set; }
+        //public DbSet<Tally> Tallies { get; set; }
     }
 }

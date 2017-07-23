fb257aadb9e081a3c790f2f33651bfe421db070b
Rogger Matamoros <roggmatz@gmail.com>
2017-07-23 09:37:09 -0400

    Initial commit. POCO used for entity framework/DB operations.

---
+++ JefeAPI/Entities/TicketEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JefeAPI.Entities
+{
+    public class TicketEntity {
+        public int id { get; set; }
+        public int GameID { get; set; }
+        public int Slot { get; set; }
+        public int Value { get; set; }
+    }
+}

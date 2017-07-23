444dcd5ea4a80e0e981e33c8679597dea96f3ee7
Rogger Matamoros <roggmatz@gmail.com>
2017-07-23 12:01:38 -0400

    Added dabatabse-related annotations to id property.

--- JefeAPI/Entities/TicketEntity.cs
+++ JefeAPI/Entities/TicketEntity.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace JefeAPI.Entities
 {
     public class TicketEntity {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
         public int GameID { get; set; }
         public int Slot { get; set; }
